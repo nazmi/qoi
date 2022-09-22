79951b217cb0f961f618ade713cf91a94fe4a791
nazmi <muhammad.binmohdropi@student.manchester.ac.uk>
2022-09-22 18:47:21 +0100

    initial commit
    work in progress
    add qoi constants
    add errors
    add colorspace and channel types

---
+++ src/errors.rs
@@ -0,0 +1,25 @@
+use std::fmt::{self , Display};
+
+pub enum Error {
+    InvalidImageDimension{width: u32, height: u32},
+    InvalidColorSpace { colorspace: u8},
+    InvalidChannels { channels: u8},
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Self::InvalidImageDimension { width, height } => {
+                write!(f, "Invalid image dimensions of {} x {}", width, height);
+            },
+            Self::InvalidColorSpace { colorspace } => {
+                write!(f, "Invalid color space: {}. Must be 0 or 1.", colorspace);
+            },
+            Self::InvalidChannels { channels } => {
+                write!(f, "Invalid number of channels: {}. Must be 3 or 4.", channels);
+            }
+        }
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,80 @@
+use crate::errors::{ Error, Result };
+
+/// The colorspace is purely informative.
+/// They do not change the way data chunks are encoded.
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug, PartialOrd, Ord)]
+pub enum ColorSpace {
+    Srgb = 0,
+    Linear = 1,
+}
+
+impl Default for ColorSpace {
+    fn default() -> Self {
+        Self::Srgb
+    }
+}
+
+// From<T> for U
+// Into<U> for T
+impl From<ColorSpace> for u8 {
+    fn from(colorspace: Colorspace) -> Self {
+        num as Self
+    }
+}
+
+impl TryFrom<u8> for ColorSpace {
+    type Error = Error;
+
+    fn try_from(colorspace: u8) -> Result<Self> {
+        match colorspace {
+            0 => Ok(Self::Srgb),
+            1 => Ok(Self::Linear),
+            _ => Err(Error::InvalidColorSpace),
+        }
+    }
+
+}
+
+/// Number of 8-bit channels in a pixel.
+/// The channel is purely informative.
+/// They do not change the way data chunks are encoded.
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug, PartialOrd, Ord)]
+pub enum Channels {
+    Rgb = 3,
+    Rgba = 4,
+}
+
+impl Channels {
+
+    /// Converts to an 8-bit integer
+    /// 3 if RGB, 4 if RGBA
+    pub const fn as_u8(self) -> u8 {
+        self as u8
+    }
+}
+
+impl Default for Channels {
+    fn default() -> Self {
+        Self::Rgb
+    }
+}
+
+// From<T> for U
+// Into<U> for T
+impl From<Channels> for u8 {
+    fn from(channels: Channels) -> Self {
+        channels as Self
+    }
+}
+
+impl TryFrom<u8> for Channels {
+    type Error = Error;
+
+    fn try_from(channels: u8) -> Result<Self> {
+        match channels {
+            3 => Ok(Self::Rgb),
+            4 => Ok(Self::Rgba),
+            _ => Err(Error::InvalidChannels { channels }),
+        }
+    }
+}
