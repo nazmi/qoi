6c1945192b6c500ecb600ca8452af9f9fbe59bfe
nazmi <muhammad.binmohdropi@student.manchester.ac.uk>
2022-09-24 21:15:49 +0100

    implement encoder specification

--- src/encode.rs
+++ src/encode.rs
@@ -1,39 +1,88 @@
-
-use crate::pixel::Pixel;
-use crate::header::Header;
 use crate::consts::*;
+use crate::error::{Error, Result};
+use crate::header::Header;
+use crate::pixel::Pixel;
+use crate::types::{Channels, ColorSpace};
 
-pub fn encode<const N: usize>(data: &[u8], header: Header) -> Option<Vec<u8>> {
+#[allow(unused_assignments)]
+fn encode_into<const N: usize>(data: &[u8], header: Header) -> Result<Vec<u8>> {
     let mut buf: Vec<u8> = Vec::new();
     buf.extend_from_slice(&header.encode());
 
     let mut run: u8 = 0;
     let mut index = [Pixel::new(); 256];
-    let mut px_prev = Pixel::<N>::new().with_a(0xff);
+    let mut px_prev = Pixel::new().with_a(0xff);
     let mut hash_prev = px_prev.hash_index();
     let mut px = Pixel::<N>::new().with_a(0xff);
     let px_end = data.len() / 4;
 
+    // todo!: vector reserve exact
+
     for (i, chunk) in data.chunks_exact(4).enumerate() {
         px.read(chunk);
+
         if px == px_prev {
             run += 1;
             if run == 62 || i == px_end - 1 {
-                buf.push(QOI_OP_RUN | (run - 1) );
+                buf.push(QOI_OP_RUN | (run - 1));
                 run = 0;
             }
-        }else{
-
+        } else {
             if run > 0 {
-                buf.push(QOI_OP_RUN | (run - 1) );
+                buf.push(QOI_OP_RUN | (run - 1));
                 run = 0;
             }
 
-            let index = px.hash_index();
-            
+            let px_rgba = Pixel::from(px, 0xff);
+            hash_prev = px.hash_index();
+            let px_index = &mut index[hash_prev as usize];
+
+            if *px_index == px_rgba {
+                buf.push(QOI_OP_INDEX | hash_prev);
+            } else {
+                *px_index = px_rgba;
+                let out = px.encode(px_prev)?;
+                buf.extend(out.into_iter());
+            }
+
+            px_prev = px;
         }
     }
-    
-    Some(buf)
+
+    buf.extend_from_slice(&QOI_PADDING);
+
+    Ok(buf)
 }
 
+pub struct Encoder<'a> {
+    data: &'a [u8],
+    header: Header,
+}
+
+impl<'a> Encoder<'a> {
+    pub fn new(data: &'a [u8], width: u32, height: u32) -> Result<Self> {
+        let mut header =
+            Header::try_new(width, height, Channels::default(), ColorSpace::default())?;
+
+        let size = data.len();
+        let n_channels = size / header.n_pixels();
+        if header.n_pixels() * n_channels != size {
+            return Err(Error::InvalidImageLength {
+                size,
+                width,
+                height,
+                n_channels,
+            });
+        }
+
+        header.channels = Channels::try_from(n_channels.min(0xff) as u8)?;
+        Ok(Self { data, header })
+    }
+
+    pub fn encode(self) -> Result<Vec<u8>> {
+        match self.header.channels {
+            Channels::Rgb => encode_into::<3>(self.data, self.header),
+            Channels::Rgba => encode_into::<4>(self.data, self.header),
+        }
+    }
+}
--- src/pixel.rs
+++ src/pixel.rs
@@ -1,21 +1,97 @@
-// use crate::error::Result;
+use crate::consts::{QOI_OP_LUMA, QOI_OP_RGB, QOI_OP_RGBA};
+use crate::error::Result;
+use core::panic;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct Pixel<const N: usize>([u8; N]);
 
 impl<const N: usize> Pixel<N> {
-
     pub const fn new() -> Self {
         Self([0; N])
     }
 
-    pub const fn with_a(mut self, a: u8) -> Self {
+    pub fn with_a(mut self, a: u8) -> Self {
         if N == 4 {
             self.0[3] = a;
         }
+
         self
     }
-    
+
+    pub fn update_rgb(&mut self, r: u8, g: u8, b: u8) {
+        self.0[0] = r;
+        self.0[1] = g;
+        self.0[2] = b;
+    }
+
+    pub fn update_rgba(&mut self, r: u8, g: u8, b: u8, a: u8) {
+        self.update_rgb(r, g, b);
+        if N == 4 {
+            self.0[3] = a;
+        }
+    }
+
+    pub fn from(self, a: u8) -> Pixel<4> {
+        let mut out = Pixel::new();
+
+        if N == 4 {
+            out.update_rgba(self.0[0], self.0[1], self.0[2], self.0[3]);
+        } else {
+            out.update_rgba(self.0[0], self.0[1], self.0[2], a);
+        }
+
+        out
+    }
+
+    pub fn read(&mut self, chunk: &[u8]) {
+        match N {
+            3 => self.update_rgb(chunk[0], chunk[1], chunk[2]),
+            4 => self.update_rgba(chunk[0], chunk[1], chunk[2], chunk[3]),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn hash_index(self) -> u8 {
+        match N {
+            3 => (self.0[0] * 3 + self.0[1] * 5 + self.0[2] * 7 + 0xff * 11) % 64,
+            4 => (self.0[0] * 3 + self.0[1] * 5 + self.0[2] * 7 + self.0[3] * 11) % 64,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn encode(&self, px_prev: Self) -> Result<Vec<u8>> {
+        let mut out = Vec::new();
+        if N == 3 || self.a_or(0) == px_prev.a_or(0) {
+            let vr = self.r().wrapping_sub(px_prev.r());
+            let vg = self.g().wrapping_sub(px_prev.g());
+            let vb = self.b().wrapping_sub(px_prev.b());
+            let vg_r = vr.wrapping_sub(vg);
+            let vg_b = vb.wrapping_sub(vg);
+            let (vr_2, vg_2, vb_2) = (vr.wrapping_add(2), vg.wrapping_add(2), vb.wrapping_add(2));
+
+            // X | 0bYY == 0bYY
+            // Return all number under 0bYY
+            // YY is bitset
+            if (vr_2 | vg_2 | vb_2 | 3) == 3 {
+                out.push(0b01 | vr_2 << 4 | vg_2 << 2 | vb_2);
+            } else {
+                let (vg_r_8, vg_b_8) = (vg_r.wrapping_add(8), vg_b.wrapping_add(8));
+                if (vg_r_8 | vg_b_8 | 15) == 15 {
+                    out.push(QOI_OP_LUMA | vg << 8);
+                    out.push(vg_r_8 << 4 | vg_r_8);
+                } else {
+                    let mut vec = vec![QOI_OP_RGB, self.r(), self.g(), self.b()];
+                    out.append(&mut vec);
+                }
+            }
+        } else {
+            let mut vec = vec![QOI_OP_RGBA, self.r(), self.g(), self.b(), self.a()];
+            out.append(&mut vec);
+        }
+
+        Ok(out)
+    }
+
     pub const fn r(self) -> u8 {
         self.0[0]
     }
@@ -28,24 +104,23 @@ impl<const N: usize> Pixel<N> {
         self.0[2]
     }
 
-    pub fn update_rgb(&mut self, r: u8, g: u8, b: u8){
-        self.0[0] = r;
-        self.0[1] = g;
-        self.0[2] = b;
-    }
-
-    pub fn update_rgba(&mut self, r: u8, g: u8, b: u8, a: u8){
-        self.update_rgb(r,g,b);
-        if N == 4 {
-            self.0[3] = a;
-        }
-    }
-
-    pub fn hash_index(self) -> u8 {
+    pub const fn a(self) -> u8 {
         match N {
-            3 => (self.0[0] * 3 + self.0[1] * 5 + self.0[2] * 7 + 0xff * 11) % 64,
-            4 => (self.0[0] * 3 + self.0[1] * 5 + self.0[2] * 7 + self.0[3] * 11) % 64,
-            _ => unreachable!(),
+            4 => self.0[3],
+            _ => panic!(),
         }
+    }
+
+    pub const fn a_or(self, value: u8) -> u8 {
+        match N {
+            4 => self.0[3],
+            _ => value,
+        }
+    }
+}
+
+impl<const N: usize> From<Pixel<N>> for [u8; N] {
+    fn from(pixel: Pixel<N>) -> Self {
+        pixel.0
     }
 }
